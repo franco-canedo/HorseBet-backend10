b2c8b49c4224c455d519106389bcb73f8a11e087
franco-canedo <francanedo1@hotmail.com>
2020-04-30 08:53:03 -0500

    log out works

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
     has_many :user_horses
     has_many :horses, through: :user_horses
     has_many :game_winners
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, presence: true, length: { minimum: 5 }
 end
