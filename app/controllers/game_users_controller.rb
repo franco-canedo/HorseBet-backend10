68905f870ac8ce4379a34e3b43921bfc3a165846
franco-canedo <francanedo1@hotmail.com>
2020-05-01 12:04:05 -0500

    waiting for others to join

--- app/controllers/game_users_controller.rb
+++ app/controllers/game_users_controller.rb
@@ -2,6 +2,26 @@ class GameUsersController < ApplicationController
     skip_before_action :authorized, only: [:join]
     def join
         game_user = GameUser.create(game_user_params)
+        
+        # game = Game.find_by(id: params[:game_id])
+        
+        # if game.users >= 2
+        #     game_user.bettable = true
+        #     game.bettable = true
+        #     game.save
+        #     game_user.save
+        #     serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        #     GameUserSerializer.new(game_user)
+        #   ).serializable_hash
+        #   GameUsersChannel.broadcast_to game, serialized_data
+        #   head :ok
+        # elsif 
+        #     serialized_data = ActiveModelSerializers::Adapter::Json.new(
+        #     GameUserSerializer.new(game_user)
+        #   ).serializable_hash
+        #   GameUsersChannel.broadcast_to game, serialized_data
+        #   head :ok
+        # end 
         render json: game_user
     end 
 
--- db/migrate/20200423192146_create_games.rb
+++ db/migrate/20200423192146_create_games.rb
@@ -6,6 +6,7 @@ class CreateGames < ActiveRecord::Migration[6.0]
       t.string :winner
       t.integer :bets_placed, :default => 0
       t.boolean :active, :default => false
+      t.boolean :bettable, :default => false
       t.boolean :joinable, :default => true
 
       t.timestamps
